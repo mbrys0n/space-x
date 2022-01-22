6bac7aafdd512d93a4b7b52706ea32ef5a4b6a94
mbrys0n <michael.bryson@live.co.uk>
2022-01-22 16:20:41 +0000

    Implement navigation for AppBar using ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Routes from 'components/Routes';
-import { AppBar, CssBaseline, ThemeProvider, Toolbar, Typography, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 const theme = createTheme();
 
@@ -8,21 +9,7 @@ function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar
-        position="absolute"
-        color="default"
-        elevation={0}
-        sx={{
-          position: 'relative',
-          borderBottom: (t) => `1px solid ${t.palette.divider}`,
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            SpaceX
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ResponsiveAppBar />
       <Routes />
     </ThemeProvider>
   );
---
+++ src/components/ResponsiveAppBar.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+const pages = { 'Upcoming Launches': '/', Favourites: '/favourites', Countdown: '/countdown' };
+//Adapted from https://mui.com/components/app-bar/#app-bar-with-responsive-menu
+
+const ResponsiveAppBar = ({ history }: RouteComponentProps): JSX.Element => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleMenuItemClick = (url: string) => () => {
+    console.log('clicked', url);
+    history.push(url);
+    handleCloseNavMenu();
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            Space-X
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {Object.entries(pages).map(([pageTitle, pageUrl]) => (
+                <MenuItem key={pageTitle} onClick={handleMenuItemClick(pageUrl)}>
+                  <Typography textAlign="center">{pageTitle}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            Space-x
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {Object.entries(pages).map(([pageTitle, pageUrl]) => (
+              <Button key={pageTitle} sx={{ my: 2, color: 'white', display: 'block' }} onClick={handleMenuItemClick(pageUrl)}>
+                {pageTitle}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default withRouter(ResponsiveAppBar);
